99cea9e35577f01bcba9f206b6ea2cec511f72d4
Seo San <dannysir@naver.com>
2023-11-10 00:57:41 +0900

    test : Order에서 메뉴 Input처리 로직 테스트 코드 Menu 가 옳바른 형식으로 왔을 때 이를 split()을 이용하여 배열로 저장하는 로직의 테스트 코드 작성

--- __tests__/DateTest.js
+++ __tests__/DateTest.js
@@ -1,4 +1,5 @@
 import Date from "../src/Model/Date.js";
+import Order from "../src/Model/Order.js";
 describe("Date Model 테스트", () => {
     test.each([10, 3])("별이 있는지 확인", async (inputs) => {
         const checkStar = new Date(inputs);
@@ -8,5 +9,13 @@ describe("Date Model 테스트", () => {
     test.each([1, 2])("별이 있는지 확인", async (inputs) => {
         const checkStar = new Date(inputs);
         await expect(checkStar.getIsStar()).toBe(false);
+    });
+
+    test("메뉴 배열로 저장", () => {
+        const MENU = "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1";
+        const isWeekEnd = true;
+        const MyOrder = new Order(MENU, isWeekEnd);
+        const ANSWER = [["티본스테이크", "1"], ["바비큐립", "1"], ["초코케이크", "2"], ["제로콜라", "1"]];
+        expect(MyOrder.getMenu()).toEqual(ANSWER);
     });
 });
