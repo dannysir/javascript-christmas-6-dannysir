1baa36eb8f9bd9c2f76220a4e2aa329a0b418284
Seo San <dannysir@naver.com>
2023-11-10 06:47:29 +0900

    fix : 테스트 코드 오류 수정 입력 값이 string이어야하는 에러와 getTotalPrice() 로직 수정으로 인한 예상 답 변경

--- __tests__/DateTest.js
+++ __tests__/DateTest.js
@@ -1,12 +1,12 @@
 import Date from "../src/Model/Date.js";
 import Order from "../src/Model/Order.js";
 describe("Date Model 테스트", () => {
-    test.each([10, 3])("별이 있는지 확인", async (inputs) => {
+    test.each(["10", "3"])("별이 있는지 확인", async (inputs) => {
         const checkStar = new Date(inputs);
         await expect(checkStar.getIsStar()).toBe(true);
     });
 
-    test.each([1, 2])("별이 있는지 확인", async (inputs) => {
+    test.each(["1", "2"])("별이 없는지 확인", async (inputs) => {
         const checkStar = new Date(inputs);
         await expect(checkStar.getIsStar()).toBe(false);
     });
@@ -23,7 +23,7 @@ describe("Date Model 테스트", () => {
         const MENU = "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1";
         const isWeekEnd = true;
         const MyOrder = new Order(MENU, isWeekEnd);
-        const ANSWER = "142,000";
-        expect(MyOrder.calculateTotalPrice(MyOrder.getMenu())).toStrictEqual(ANSWER);
+        const ANSWER = 142000;
+        expect(MyOrder.getTotalPrice()).toStrictEqual(ANSWER);
     });
 });
