1f401cb0d0ee0f5107178fe944c1599a4b967e2e
Seo San <dannysir@naver.com>
2023-11-09 22:26:58 +0900

    test : Date 클래스의 별 확인 기능 Date class에서 날짜를 입력하면 그 날이 별이 있는 날인지 확인

---
+++ __tests__/DateTest.js
@@ -0,0 +1,12 @@
+import Date from "../src/Model/Date.js";
+describe("Date Model 테스트", () => {
+    test.each([10, 3])("별이 있는지 확인", async (inputs) => {
+        const checkStar = new Date(inputs);
+        await expect(checkStar.getIsStar()).toBe(true);
+    });
+
+    test.each([1, 2])("별이 있는지 확인", async (inputs) => {
+        const checkStar = new Date(inputs);
+        await expect(checkStar.getIsStar()).toBe(false);
+    });
+});
