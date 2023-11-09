c22492014dc2ec5338ffa65127a2927a88be0bdc
Seo San <dannysir@naver.com>
2023-11-10 01:44:16 +0900

    feat : OutputView 메뉴를 받아서 출력, 날짜에 맞게 문구 출력

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -1,10 +1,13 @@
 import {Console} from "@woowacourse/mission-utils";
 
 const OutputView = {
-    printMenu(san) {
-        Console.print(san);
-        // ...
-    }
+    printMenu(Menu) {
+        Console.print("<주문 메뉴>");
+        Console.print(Menu);
+    },
+    printFisrtOut(Day) {
+        Console.print(`12월 ${Day}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n`);
+    },
     // ...
 }
 export default OutputView;
