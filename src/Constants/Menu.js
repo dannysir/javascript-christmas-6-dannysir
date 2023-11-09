2bb6172119c1953bc09c05c7164b19939f02f8fb
Seo San <dannysir@naver.com>
2023-11-10 02:28:30 +0900

    feat : Menu 객체 구조 변경 type으로 종류를 구분하도록 변경

--- src/Constants/Menu.js
+++ src/Constants/Menu.js
@@ -1,24 +1,23 @@
-const Menu = {
-    appetizer: [
-        { name: "양송이수프", price: 6000 },
-        { name: "타파스", price: 5500 },
-        { name: "시저샐러드", price: 8000 },
-    ],
-    main: [
-        { name: "티본스테이크", price: 55000 },
-        { name: "바비큐립", price: 54000 },
-        { name: "해산물파스타", price: 35000 },
-        { name: "크리스마스파스타", price: 25000 },
-    ],
-    dessert: [
-        { name: "초코케이크", price: 15000 },
-        { name: "아이스크림", price: 5000 },
-    ],
-    drink: [
-        { name: "제로콜라", price: 3000 },
-        { name: "레드와인", price: 60000 },
-        { name: "샴페인", price: 25000 },
-    ],
-};
-console.log(Menu.appetizer.some((item) => item.name ==="시저샐러드"));
+const Menu = [
+
+    {name: "양송이수프", price: 6000, type: "appetizer"},
+    {name: "타파스", price: 5500, type: "appetizer"},
+    {name: "시저샐러드", price: 8000, type: "appetizer"},
+
+
+    {name: "티본스테이크", price: 55000, type: "main"},
+    {name: "바비큐립", price: 54000, type: "main"},
+    {name: "해산물파스타", price: 35000, type: "main"},
+    {name: "크리스마스파스타", price: 25000, type: "main"},
+
+
+    {name: "초코케이크", price: 15000, type: "dessert"},
+    {name: "아이스크림", price: 5000, type: "dessert"},
+
+    {name: "제로콜라", price: 3000, type: "drink"},
+    {name: "레드와인", price: 60000, type: "drink"},
+    {name: "샴페인", price: 25000, type: "drink"},
+
+];
+console.log(Menu.some((item) => item.name === "시저샐러드"));
 export default Menu;
