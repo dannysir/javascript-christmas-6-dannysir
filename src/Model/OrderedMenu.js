486d3bb22a6257c52170d5f1c809515c2ce4b155
Seo San <dannysir@naver.com>
2023-11-10 21:51:45 +0900

    feat : 주문 갯수 0입력 예외 처리 주문 갯수에 1 미만의 값을 입력할 경우 예외 처리 발생 로직 추가

--- src/Model/OrderedMenu.js
+++ src/Model/OrderedMenu.js
@@ -20,6 +20,8 @@ class OrderedMenu {
         OrderArray[1].split("").forEach(v => {
             if (isNaN(v)) throw new Error("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         });
+        if (parseInt(OrderArray[1]) < 1) throw new Error("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+
     }
     checkType(OrderArr) {
         return  Menu.find(({name}) => name === OrderArr[0]).type;
