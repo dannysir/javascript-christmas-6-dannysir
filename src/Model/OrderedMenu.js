88303a016f30d059f4964512ce04dcf70d0aa125
Seo San <dannysir@naver.com>
2023-11-10 20:40:09 +0900

    feat : 주문 유효성 검사 각각의 주문이 메뉴에 있는 주문인지, 숫자를 입력했는지 확인

--- src/Model/OrderedMenu.js
+++ src/Model/OrderedMenu.js
@@ -6,12 +6,21 @@ class OrderedMenu {
     #TYPE
     #PRICE
     constructor(OrderArray) {
+        this.validator(OrderArray);
         this.#NAME = OrderArray[0];
         this.#COUNT = parseInt(OrderArray[1]);
         this.#TYPE = this.checkType(OrderArray);
         this.#PRICE = this.checkPrice(OrderArray);
     }
 
+    validator(OrderArray) {
+        if (!Menu.some((v) => v.name === OrderArray[0])) {
+            throw new Error("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+        OrderArray[1].split("").forEach(v => {
+            if (isNaN(v)) throw new Error("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+    }
     checkType(OrderArr) {
         return  Menu.find(({name}) => name === OrderArr[0]).type;
     }
