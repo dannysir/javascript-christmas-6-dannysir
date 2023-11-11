fd2a5a4658f29174f10c5a12d2f3d3b58e47dfff
Seo San <dannysir@naver.com>
2023-11-11 14:00:16 +0900

    test : 날짜 입력 예외처리 1부터 31이 아닌 숫자, 소숫점 입력, 숫자가 아닌 문자 입력시 예외 처리 테스트코드 작성

--- __tests__/DateTest.js
+++ __tests__/DateTest.js
@@ -1,4 +1,5 @@
 import Date from "../src/Model/Date.js";
+import Order from "../src/Model/Order.js";
 describe("Date Model 테스트", () => {
     test.each(["10", "3"])("별이 있는지 확인", async (inputs) => {
         const checkStar = new Date(inputs);
@@ -10,4 +11,9 @@ describe("Date Model 테스트", () => {
         await expect(checkStar.getIsStar()).toBe(false);
     });
 
+    test.each(["0", "41", "1.1", "1a"])("예외처리 - 옳지 않은 입력", (inputs) => {
+        expect(() => {
+            new Date(inputs)
+        }).toThrow("[ERROR]");
+    });
 });
